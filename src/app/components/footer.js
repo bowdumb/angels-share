20dfce71f49b5b4d00464e98beeffe3fd95d2ded
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-03-25 11:12:01 -0400

    Initial commit for new repository.

---
+++ src/app/components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-700 text-white text-center p-4">
+            This is the footer
+        </footer>
+    );
+}
---
+++ src/app/components/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="bg-blue-500 text-white text-end p-8">
+            <h1 className="text-4xl font-bold">Angel's Share Bar Book</h1>
+            <p className="mt-2">Because Vince said so!</p>
+        </header>
+    );
+}
