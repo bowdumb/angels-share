525a1c7271b3d040841ca992f290032d01c59f35
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-03-26 11:48:23 -0400

    Added further styling for navbar links.

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <header className="bg-blue-500 text-white text-start p-8">
             <h1 className="text-4xl font-bold">Angel's Share Bar Book</h1>
             <p className="mt-2">Because Vince said so!</p>
-            <div className="text-white text-end underline">
+            <div className="text-white text-end underline hover:text-blue-300">
                 <Link href="/about" >
                     About
                 </Link>
