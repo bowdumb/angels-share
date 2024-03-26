444ec09d3199da4f08af4f2621477bbea3af294f
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-03-26 11:33:45 -0400

    Added rudimentary Tailwind styling for About link within Header.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,4 +1,4 @@
-export default function () {
+export default function About () {
     return (
         <div>
             <p>This will be the about page!</p>
--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,8 +1,15 @@
+import Link from 'next/link';
+
 export default function Header() {
     return (
         <header className="bg-blue-500 text-white text-start p-8">
             <h1 className="text-4xl font-bold">Angel's Share Bar Book</h1>
             <p className="mt-2">Because Vince said so!</p>
+            <div className="text-white text-end underline">
+                <Link href="/about" >
+                    About
+                </Link>
+            </div>
         </header>
     );
 }
