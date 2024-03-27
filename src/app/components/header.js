dc025c48fa47e022a91da3f2607e9794a385606c
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-03-27 01:11:44 -0400

    Added some styling and formatting to the header/nav component. Gave the nav links spacing, and created two containers for the title/subtitle and the nav links themselves.

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -2,14 +2,19 @@ import Link from 'next/link';
 
 export default function Header() {
     return (
-        <header className="bg-blue-500 text-white text-start p-8">
-            <h1 className="text-4xl font-bold">Angel's Share Bar Book</h1>
-            <p className="mt-2">Because Vince said so!</p>
-            <div className="text-white text-end underline hover:text-blue-300">
-                <Link href="/about" >
-                    About
-                </Link>
+        <header className="bg-blue-500 text-white p-8 flex flex-wrap justify-between items-start">
+            <div>
+                <h1 className="text-4xl font-bold my-1">Angel's Share Bar Book</h1>
+                <p className="mt-2">Because Vince said so!</p>
             </div>
+            <nav className="flex space-x-12 mt-12 pt-7">
+                <div className="text-white text-end underline hover:text-blue-300">
+                    <Link href="/search">Search</Link>
+                </div>
+                <div className="text-white text-end underline hover:text-blue-300">
+                    <Link href="/about" >About</Link>
+                </div>
+            </nav>
         </header>
     );
 }
