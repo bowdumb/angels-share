82812ee70eb4a5eaa12ec21d8ead20627107b5f4
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-03-28 10:12:09 -0400

    Imported SearchBar component into the search page body.

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,7 +1,10 @@
+import SearchBar from '../components/searchBar'
+
 export default function Search() {
     return (
         <div>
             This is the search page.
+            <SearchBar />
         </div>
     )
 }
