d55afe699e7cd6a96f44f26a2f0b56695d6fd0ee
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-04-27 22:25:00 -0400

    Added conditional rendering that will render text telling a user that a match in the database was not found for their search.

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -31,31 +31,37 @@ export default function Search() {
     <div>
       <SearchBar defaultValue={searchQuery} />
       <div>
-        {results.map(result => (
-          <div key={result._id}>
-            <h1 className="text-xl font-bold pt-10">{result.name}</h1>
-            <h2 className="text-lg font-semibold mt-4">Ingredients:</h2>
-            <ul className="list-disc pl-5">
-              {result.ingredients.map((ingredient, index) => (
-                <li key={index} className="ml-4">{ingredient}</li>
-              ))}
-            </ul>
-            <h2 className="text-lg font-semibold mt-4">Glassware:</h2>
+        {results.length > 0 ? (
+          results.map(result => (
+            <div key={result._id}>
+              <h1 className="text-xl font-bold pt-10">{result.name}</h1>
+              <h2 className="text-lg font-semibold mt-4">Ingredients:</h2>
+              <ul className="list-disc pl-5">
+                {result.ingredients.map((ingredient, index) => (
+                  <li key={index} className="ml-4">{ingredient}</li>
+                ))}
+              </ul>
+              <h2 className="text-lg font-semibold mt-4">Glassware:</h2>
               <p className="pl-5">{result.glassware}</p>
-            <h2 className="text-lg font-semibold mt-4">Instructions: </h2>
-            <ol className="list-decimal pl-5">
-            {result.instructions.map((instructions, index) => (
-              <li key={index} className="ml-4">{instructions}</li>
-            ))}
-            </ol>
-            {result.optional && (
-              <>
-                <h2 className="text-lg font-semibold mt-4">Optional: </h2>
-                <p className="pl-5">{result.optional}</p>
-              </>
-            )}
+              <h2 className="text-lg font-semibold mt-4">Instructions:</h2>
+              <ol className="list-decimal pl-5">
+                {result.instructions.map((instruction, index) => (
+                  <li key={index} className="ml-4">{instruction}</li>
+                ))}
+              </ol>
+              {result.optional && (
+                <div>
+                  <h2 className="text-lg font-semibold mt-4">Optional:</h2>
+                  <p className="pl-5">{result.optional}</p>
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <div className="text-xl font-bold ççtext-red-500 text-center mt-10">
+            No results found for your search.
           </div>
-        ))}
+        )}
       </div>
     </div>
   )};
