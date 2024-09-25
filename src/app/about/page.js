d9f923397a5d9d58a18d2b79e846f4fe1e1b2670
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-09-25 09:47:49 -0400

    Added some brief placeholder content for the About page.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,11 @@
 export default function About () {
     return (
         <div>
-            <p>This will be the about page!</p>
+            <h1 className="text-2xl font-bold">About Angel's Share</h1>
+                <p>Angel's share was created with a simple goal in mind: to take the frail, water-damaged bar book that exists behind every bar and to 
+                    put it on the Internet. I was tired of pages falling out, drinks getting spilled on the pages itself, and inconsistencies in the recipes
+                     found between multiple copies of the book. From there the concept involved to include community contribution, collaboration, and communication..
+                </p>
         </div>
     )
 }
